478f557a68ec577363fa2c5f7b1215cc5d3bf8a9
hasYm8 <laszlo.peter.zoldi@gmail.com>
2025-04-19 21:26:52 +0200

    Deleted unused frontend stuff

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,29 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { Book, BookService } from '../../services/book.service';
-import { BookCardComponent } from '../../components/book-card/book-card.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, BookCardComponent],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-
-  private bookService = inject(BookService);
-  books: Book[] = [];
-
-  ngOnInit(): void {
-    this.getBooks();
-  }
-
-  getBooks() {
-    this.bookService.getBooks()
-      .subscribe({
-        next: (res) => {
-          this.books = res.data;
-        },
-      })
-  }
+export class HomeComponent {
 }
